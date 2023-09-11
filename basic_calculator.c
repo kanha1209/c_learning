e3172630d235f294797faefc9d829e685ef3fbaa
kanha1209 <144531358+kanha1209@users.noreply.github.com>
2023-09-11 21:52:48 +0530

    Update basic_calculator.c

--- basic_calculator.c
+++ basic_calculator.c
@@ -16,6 +16,12 @@ void main() {
     printf("Enter second number: ");
     scanf("%d", &num2);
     
-    int answer = num1 + num2;
-    printf("Addition of two numbers is %d", answer);
+    if (sign == '+') {
+        printf("You entered a plus sign.\n");
+        int answer = num1 + num2;
+        printf("Addition of two numbers is %d", answer);
+    } else {
+        printf("Invalid operation.\n");
+    }
+    
 }
