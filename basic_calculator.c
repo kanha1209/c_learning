21a8dfc6c98fe5ad59edf5fec7c128828a05ea78
kanha1209 <144531358+kanha1209@users.noreply.github.com>
2023-09-11 22:08:22 +0530

    Update basic_calculator.c

--- basic_calculator.c
+++ basic_calculator.c
@@ -20,6 +20,18 @@ void main() {
         printf("You entered a plus sign.\n");
         int answer = num1 + num2;
         printf("Addition of two numbers is %d", answer);
+    } else if (sign == '-') {
+        printf("You entered a minus sign.\n");
+        int answer = num1 - num2;
+        printf("Substraction of two numbers is %d", answer);
+    } else if (sign == '/') {
+        printf("You entered a divide sign.\n");
+        int answer = num1 / num2;
+        printf("Dividing of two numbers is %d", answer);
+    } else if (sign == '*') {
+        printf("You entered a multiply sign.\n");
+        int answer = num1 * num2;
+        printf("Multiplication of two numbers is %d", answer);
     } else {
         printf("Invalid operation.\n");
     }
